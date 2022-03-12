7aef84aca232b338c1cceae05a48db33ac620d9e
Mathew Heard <splitice@users.noreply.github.com>
2022-03-12 17:31:27 +1100

    remove debug statements and document requirements

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ SWAP_PCT: 0.7 # 70% high water mark
 ```
 
 To enable push as priveledged side car with sysfs mount
+
+Sidecar requires:
+ - priveledged
+ - /sys mount
+ - shared process namespace
+ - kubetrnetes configured without failOnSwap
+
+
+Tested with Docker container engine
--- container/entry.sh
+++ container/entry.sh
@@ -6,17 +6,13 @@ fi
 
 function do_cgroup {
     docker_container=$(cat /proc/1/cgroup | sed 's/0::\/..\///')
-    echo "docker container: $docker_container"
     cgpath=$(find /sys/fs/cgroup -type d -iname $docker_container)"/../"
-    echo "cgroup path: $cgpath"
 
     for f in "$cgpath"*/memory.swap.max; do
             fbase=$(dirname "$f")
             memlimit=$(cat "$fbase/memory.max")
             if [[ "$memlimit" != "max" ]]; then
-                    echo "memory.max: $memlimit"
                     memory_high=$(awk -vp=$memlimit 'BEGIN{printf "%d" , p*'$SWAP_PCT'}')
-                    echo "memory.high: $memory_high"
                     echo "$memory_high" > $fbase/memory.high
                     echo "max" > $fbase/memory.swap.max
             fi

e2608be9f80c79c2846bc69a0d088b1ef69fd19d
Mathew <mat999@gmail.com>
2024-10-22 13:15:23 +1100

    add debugging statements

--- container/entry.sh
+++ container/entry.sh
@@ -6,7 +6,16 @@ fi
 
 function do_cgroup {
     docker_container=$(cat /proc/1/cgroup | sed 's/0::.\+\/\(.\+\)/\1/')
+    if [[ -z "$docker_container" ]]; then
+            echo "Could not determine container group base"
+            return
+    fi
+
     cgpath=$(find /sys/fs/cgroup -type d -iname $docker_container)"/../"
+    if [[ ! -d "$cgpath" ]]; then
+            echo "Cgroup path not found for $docker_container"
+            return
+    fi
 
     for f in "$cgpath"*/memory.swap.max; do
             echo "Setting cgroup: $f"
